cf3f6a8e920c807eda5ee15498120f4c6df79990
Gabriel Eric <gabriel.eric@ufrpe.br>
2025-11-24 16:57:28 -0300

    :sparkles: feat: Configurado para gcloud run

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from routes.usuario import usuario_bp
 from routes.sistema import sistema_bp
 from routes.sistemausuario import sistema_usuario_bp
 from routes.auth import auth_bp
+import os
 
 app.register_blueprint(usuario_bp, url_prefix='/usuarios')
 app.register_blueprint(sistema_bp, url_prefix='/sistemas')
 app.register_blueprint(sistema_usuario_bp, url_prefix='/sistemas-usuario')
 app.register_blueprint(auth_bp, url_prefix='/validate')
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5500))
+    app.run(host="0.0.0.0", port=port))
